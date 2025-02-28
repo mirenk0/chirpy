42354efa44be2be20470b85be17c54067bec1c46
mirenk0 <MirkoDSov@gmail.com>
2025-02-28 16:10:40 +0100

    Basic fileserver

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("."))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	mux.Handle("/", fileServer)
 
 	server.ListenAndServe()
 }
