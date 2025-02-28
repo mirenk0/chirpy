9d5b4d25a8807cc6c3cd784f3069785fd1710d34
mirenk0 <MirkoDSov@gmail.com>
2025-02-28 16:30:49 +0100

    Readiness endpoint

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	fileServer := http.FileServer(http.Dir("."))
 
@@ -14,8 +17,18 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 	mux.Handle("/assets/logo.png", fileServer)
 
-	server.ListenAndServe()
+	fmt.Println("Starting server on :8080...")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
 }
