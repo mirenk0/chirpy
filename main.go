4385821a1e66b747c4ed3ce6cf66ac4371c4dd8a
mirenk0 <MirkoDSov@gmail.com>
2025-03-01 16:26:51 +0100

    Add Routing Methods for decoupling our monolith with /api/ path for all non-fileserver routes

--- main.go
+++ main.go
@@ -14,14 +14,14 @@ func main() {
 	mux := http.NewServeMux()
 	apiCfg := &apiConfig{}
 
-	mux.HandleFunc("GET /healthz", readinessHandler)
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
 
 	fileServer := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
-	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("GET /api/metrics", apiCfg.metricsHandler)
 
-	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/reset", apiCfg.resetHandler)
 
 	mux.Handle("/assets/logo.png", fileServer)
 
